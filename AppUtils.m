e84d8be87c17727e4293ea8d34a26a90319d16fb
Weberson <weberson@516680.com>
2016-07-26 16:06:45 +0800

    基本的项目工具

---
+++ AppUtils.m
@@ -0,0 +1,208 @@
+//
+//  AppUtils.m
+//  GoodDemo_Weberson
+//
+//  Created by Weberson on 15/9/15.
+//  Copyright (c) 2015年 Weberson. All rights reserved.
+//
+
+#import "AppUtils.h"
+
+@implementation AppUtils
+
+//获取app的版本号
++ (NSString *)getMyApplicationVersion{
+    NSDictionary *info = [[NSBundle mainBundle] infoDictionary];
+    NSString *version = [info objectForKey:@"CFBundleShortVersionString"];
+    return version;
+}
+
+/*******************--- 时间问题 ---*********************/
+//根据字符串获取到NSdate类型的数据
++ (NSDate *)getDateFromString:(NSString *)string{
+    NSDateFormatter * formatter = [[NSDateFormatter alloc]init];
+    [formatter setDateFormat:@"yyyy-MM-dd-HH-mm-ss"];
+    NSDate *date = [formatter dateFromString:string];
+    return date;
+}
+
+//根据NSDate类型转换成字符串
++ (NSString *)getStringFromNSDate:(NSDate *)date{
+    NSDateFormatter * formatter = [[NSDateFormatter alloc]init];
+    [formatter setDateFormat:@"yyyy-MM-dd-HH-mm-ss"];
+    NSString *str= [formatter stringFromDate:date];
+    return str;
+}
+
+/*********************--保存和取出数据-NSUserDefaults---******************/
++ (void)setValue:(id)valuem key:(NSString *)key{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:valuem forKey:key];
+    [defaults synchronize];
+}
+
++ (void)setBool:(BOOL)boo key:(NSString *)key{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setBool:boo forKey:key];
+    [defaults synchronize];
+}
+
++ (id)ObjectforKey:(NSString *)key{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    return [defaults objectForKey:key];
+}
+
++ (BOOL)BooltforKey:(NSString *)key{
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    return [defaults boolForKey:key];
+}
+
+/********************* SVProgressHUD **********************/
++ (void)showSuccessMessage:(NSString *)message{
+    //想设置一些信息可以选择custom类型的 具体看api
+    [SVProgressHUD setDefaultStyle:SVProgressHUDStyleDark];
+    [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeBlack];
+    [SVProgressHUD showSuccessWithStatus:message];
+}
+
++ (void)showErrorMessage:(NSString *)message{
+    [SVProgressHUD setDefaultStyle:SVProgressHUDStyleDark];
+    [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeBlack];
+    [SVProgressHUD showErrorWithStatus:message];
+}
+
++ (void)showProgressMessage:(NSString *) message{
+    [SVProgressHUD setDefaultStyle:SVProgressHUDStyleDark];
+    [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeBlack];
+    [SVProgressHUD showWithStatus:message];
+}
+
++ (void)showWithStatus:(NSString *)message{
+    [SVProgressHUD setDefaultStyle:SVProgressHUDStyleDark];
+    [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeBlack];
+    [SVProgressHUD showWithStatus:message];
+}
+
++ (void)showInfoWithStatus:(NSString*)message{
+    
+    [SVProgressHUD setDefaultStyle:SVProgressHUDStyleDark];
+    [SVProgressHUD setDefaultMaskType:SVProgressHUDMaskTypeBlack];
+    [SVProgressHUD showWithStatus:message];
+}
+
++ (void)dismissHUD{
+    [SVProgressHUD dismiss];
+}
+
+/*******************--检测--******************************/
+//验证手机合法性
++ (BOOL)checkPhoneNumber:(NSString *)phoneNumber{
+    
+    NSString * MOBILE = @"^1(3[0-9]|5[0-35-9]|8[025-9])\\d{8}$";
+    NSString * CM = @"^1(34[0-8]|(3[5-9]|5[017-9]|8[278])\\d)\\d{7}$";
+    NSString * CU = @"^1(3[0-2]|5[256]|8[56])\\d{8}$";
+    NSString * CT = @"^1((33|53|8[09])[0-9]|349)\\d{7}$";
+    
+    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
+    NSPredicate *regextestcm = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CM];
+    NSPredicate *regextestcu = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CU];
+    NSPredicate *regextestct = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CT];
+    BOOL res1 = [regextestmobile evaluateWithObject:phoneNumber];
+    BOOL res2 = [regextestcm evaluateWithObject:phoneNumber];
+    BOOL res3 = [regextestcu evaluateWithObject:phoneNumber];
+    BOOL res4 = [regextestct evaluateWithObject:phoneNumber];
+    
+    if (res1 || res2 || res3 || res4 )
+    {
+        return YES;
+    }
+    else
+    {
+        return NO;
+    }
+}
+
+//检测空格是不是空 或者  是空格
++ (BOOL)isBlankString:(NSString *)string{
+    
+    if (string == nil) {
+        return YES;
+    }
+    
+    if (string == NULL) {
+        return YES;
+    }
+    
+    if ([string isKindOfClass:[NSNull class]]) {
+        return YES;
+    }
+    
+    if ([[string stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] length]==0) {
+        return YES;
+    }
+    return NO;
+}
+
+//随机颜色
++ (UIColor *)randomColor{
+    CGFloat red=arc4random()%256/255.0;
+    CGFloat green=arc4random()%256/255.0;
+    CGFloat blue=arc4random()%256/255.0;
+    UIColor *randomColor=[UIColor colorWithRed:red green:green blue:blue alpha:1.0];
+    return randomColor;
+}
+
+
+
+/// 占位文字 文字大小 文字颜色
+-(void)textField:(UITextField *)textField FontSize:(CGFloat)fontSize placeHolderTextColor:(UIColor *)textColor{
+    
+    [textField setValue:[UIFont systemFontOfSize:fontSize] forKeyPath:@"_placeholderLabel.font"];
+    
+    if (textColor != nil) {
+        [textField setValue: textColor forKeyPath:@"_placeholderLabel.textColor"];
+    }
+}
+
+/// 检测是否是字母
++(BOOL)isAlphabetWithstring:(NSString *)string{
+    NSString* number=@"^[A-Za-z]+$";
+    NSPredicate *numberPre = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",number];
+    return  [numberPre evaluateWithObject:string];
+}
+
+
+/****************************       随笔记录一下         ******************************
+ 
+ PM是个温柔的SB
+ 
+ 〇// 定义公共key值
+ //    NSString *key = (NSString *)kCFBundleVersionKey;
+ //    //plist里面的 "Build" 的号
+ //    NSString *currentVersionCode = [NSBundle mainBundle].infoDictionary[key];
+ 
+ ①： 定义公共key值
+ //    NSString *key = (NSString *)kCFBundleVersionKey;
+ //    //plist里面的 "Build" 的号
+ //    NSString *currentVersionCode = [NSBundle mainBundle].infoDictionary[key];
+ 
+ ②: 设置 TextField 占位文字的大小和颜色
+ [lastNameTfd setValue:[UIFont systemFontOfSize:14] forKeyPath:@"_placeholderLabel.font"];
+ [lastNameTfd setValue:[UIColor redColor] forKeyPath:@"_placeholderLabel.textColor"];
+ 
+ ③: 设置导航条 图片渲染模式
+ 
+ UIImage *shareImage = [UIImage imageNamed:@"naviShare@2x"];
+ UIBarButtonItem *shareButton = [[UIBarButtonItem alloc]initWithImage:shareImage style:UIBarButtonItemStyleDone target:self action:@selector(shareButtonDidClick)];
+ shareButton.image = [shareImage imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+ 
+ ④: 设置导航条 白色返回按钮
+ // 统一设置左侧的返回按钮 用图片代替
+ UIImage *backImage = [UIImage imageNamed:@"getback@3x"];
+ [[UIBarButtonItem appearance] setBackButtonBackgroundImage:[backImage resizableImageWithCapInsets:UIEdgeInsetsMake(0, backImage.size.width, 0, 0)]forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+ [[UIBarButtonItem appearance] setBackButtonTitlePositionAdjustment:UIOffsetMake(-233, 0) forBarMetrics:UIBarMetricsDefault];
+ 
+ ****************************       随笔记录信息         ******************************/
+
+
+@end
